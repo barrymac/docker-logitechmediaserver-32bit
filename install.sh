e0d178c4fee4dd7b372c56623465b39f0c1b8a05
barry <barrymac@gmail.com>
2015-01-06 21:17:27 +0000

    added docker files

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#########################################
+##        ENVIRONMENTAL CONFIG         ##
+#########################################
+
+# Configure user nobody to match unRAID's settings
+usermod -u 99 nobody
+usermod -g 100 nobody
+usermod -d /home nobody
+chown -R nobody:users /home
+
+# Disable SSH
+rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
+
+#########################################
+##    REPOSITORIES AND DEPENDENCIES    ##
+#########################################
+
+# Repositories
+sh -c "echo deb http://debian.slimdevices.com stable main >> /etc/apt/sources.list"
+
+# Install Dependencies
+apt-get update -qq
+apt-get install -qy --force-yes logitechmediaserver
+apt-get install -qy lame
+
+#########################################
+##                 CLEANUP             ##
+#########################################
+
+# Clean APT install files
+apt-get clean -y
+rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/*
